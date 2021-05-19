148c146c1ea0818e2971050b2222bd067ffabc8d
Alexandre Martuscelli Faria <martuscellifaria@gmail.com>
2021-05-19 14:24:18 -0300

    Update starman_test.py

--- src/python_integration_example/starman_test.py
+++ src/python_integration_example/starman_test.py
@@ -5,18 +5,18 @@ import os
 load_dotenv()
 
 #For Starman
-app_name = 'integração propensity ga'
+app_name = 'app_test'
 username = os.getenv('username')
 password = os.getenv('password')
 
 
 def starman_call (urgency, msg_body):
-    requests.post(url='https://starman-bot-ppw7yxqtsa-uc.a.run.app/starman/api/v1.0/',
+    requests.post(url='http://0.0.0.0:5000/starman/api/v1.0/',
     json={'message': msg_body, 'urgency': urgency, 'app_name': app_name},
     auth=(username, password))
     return 'Message sent!'
 
 
-#msg_body = f'Teste Request Python'
-#urgency = 'urgent'
-#starman_call(urgency, msg_body)
+msg_body = f'Request Python'
+urgency = 'urgent'
+starman_call(urgency, msg_body)
