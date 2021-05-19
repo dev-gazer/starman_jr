267299dea3a1d0892f8468b884effb57d5db2dee
Alexandre Martuscelli Faria <martuscellifaria@gmail.com>
2021-05-19 14:27:13 -0300

    Update starman_test.php

--- src/php_integration_example/starman_test.php
+++ src/php_integration_example/starman_test.php
@@ -4,25 +4,25 @@ require __DIR__.'/vendor/autoload.php';
 use GuzzleHttp\Client;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-/*
-$app_name = 'identified sale';
+
+$app_name = 'your_app_name';
 $username = $_ENV['username'];
 $password = $_ENV['password'];
 $urgency = 'urgent';
-$msg_body = 'Teste Guzzle PHP';
+$msg_body = 'Guzzle PHP Test';
 
-$client = new Client(['base_uri' => 'https://starman-bot-ppw7yxqtsa-uc.a.run.app/']);
+$client = new Client(['base_uri' => 'http://0.0.0.0:5000/']);
   
-$response = $client->request('POST', 'starman/api/v1.0/', ['auth' => [$username, $password],
+$response = $client->request('POST', 'starman_jr/api/v1.0/', ['auth' => [$username, $password],
 'json' => ['message' => $msg_body, 'urgency' => $urgency, 'app_name' => $app_name]]);
-*/
 
-$msg_body = 'Teste de email via request em php.';
-$subject = 'Teste';
-$app_name = 'identified sale';
+
+$msg_body = 'E-Mail test.';
+$subject = 'Test';
+$app_name = 'your_app_name';
 $username = $_ENV['username'];
 $password = $_ENV['password'];
-$mailto = array('martuscellifaria@gmail.com', 'martuscellifaria@usp.br', 'menttheband@gmail.com');
+$mailto = array('email@provider.com');
 
 $client = new Client(['base_uri' => 'http://0.0.0.0:5000/']);
 
